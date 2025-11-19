b896e9e735cb5d975fbd8af39a1a4538923a4271
isamu <isamu@to-kyo.to>
2025-11-19 12:27:27 +0900

    udpate

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-10">
+  <div class="p-10" v-if="data">
     <template v-for="v in data.files" :key="v">
       <li>
         <router-link :to="`/contents/${v}/0`">{{ v }}</router-link>
@@ -9,6 +9,21 @@
 </template>
 
 <script setup lang="ts">
-import { data } from "./data";
-console.log(data);
+import { ref } from "vue";
+import { BASEURL } from "../configs/config";
+
+const data = ref<{ files: string[] } | null | undefined>(undefined);
+const main = async () => {
+  try {
+    const res = await fetch(BASEURL + "data.json");
+    if (res.status === 200) {
+      data.value = (await res.json()) as ViewerData;
+      return;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  data.value = null;
+};
+main();
 </script>
